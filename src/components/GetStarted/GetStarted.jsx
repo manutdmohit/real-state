a63518e07ea045c61a3c13a4ed5be1665e2697b4
Mohit Saud <mohit@prabidhilabs.com>
2024-02-16 18:20:53 +0545

    Update mail

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,18 +1,20 @@
-import React from "react";
-import "./GetStarted.css";
+import React from 'react';
+import './GetStarted.css';
 const GetStarted = () => {
   return (
     <div id="get-started" className="g-wrapper">
       <div className="paddings innerWidth g-container">
         <div className="flexColCenter inner-container">
-          <span className="primaryText">Get started with Homyz</span>
+          <span className="primaryText">
+            Get started with Eco Finance & Home Loans
+          </span>
           <span className="secondaryText">
             Subscribe and find super attractive price quotes from us.
             <br />
             Find your residence soon
           </span>
           <button className="button" href>
-            <a href="mailto:zainkeepscode@gmail.com">Get Started</a>
+            <a href="mailto:ecofinance23@gmail.com">Get Started</a>
           </button>
         </div>
       </div>
