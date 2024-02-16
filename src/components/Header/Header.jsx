4b84826729419d7359537e5ada210f650cb566c6
Mohit Saud <mohit@prabidhilabs.com>
2024-02-16 17:38:35 +0545

    Update main logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
       <div className="flexCenter innerWidth paddings h-container">
         {/* logo */}
         <Link to="/">
-          <img src="./logo_main.png" alt="logo" width={100} id="logo" />
+          <img src="./main_logo.png" alt="logo" width={100} id="logo" />
         </Link>
 
         {/* menu */}
