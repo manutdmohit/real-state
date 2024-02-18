1fe87abe10363d780b3a699713270c60333415c8
Mohit Saud <mohit@prabidhilabs.com>
2024-02-18 09:56:53 +0545

    Remove UserContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -26,29 +26,27 @@ function App() {
   });
 
   return (
-    <UserDetailContext.Provider value={{ userDetails, setUserDetails }}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route element={<Layout />}>
-                <Route path="/" element={<Website />} />
-                <Route path="/properties">
-                  <Route index element={<Properties />} />
-                  <Route path=":propertyId" element={<Property />} />
-                </Route>
-                <Route path="/contacts" element={<Contact />}></Route>
-                <Route path="/bookings" element={<Bookings />} />
-                <Route path="/favourites" element={<Favourites />} />
-                <Route path="*" element={<NotFound />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Website />} />
+              <Route path="/properties">
+                <Route index element={<Properties />} />
+                <Route path=":propertyId" element={<Property />} />
               </Route>
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
-        <ToastContainer />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </UserDetailContext.Provider>
+              <Route path="/contacts" element={<Contact />}></Route>
+              <Route path="/bookings" element={<Bookings />} />
+              <Route path="/favourites" element={<Favourites />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+      <ToastContainer />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
